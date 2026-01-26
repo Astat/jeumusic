7180266cd32487eef75f890ce4a07a65b3287163
Astat <alban.pipon@gmail.com>
2026-01-26 21:21:56 +0100

    Create config.js

---
+++ sounds/Cordes/Exercice1/config.js
@@ -0,0 +1,68 @@
+// ============================================
+// CONFIGURATION - Edit this list to add/remove exercises
+// ============================================
+const EXERCISES = [
+    // CORDES (5 sounds)
+    {
+        exerciseName: "Cordes - Exercice 1",
+        sounds: {
+            1: 'sounds/Cordes/Exercice1/1.m4a',
+            2: 'sounds/Cordes/Exercice1/2.m4a',
+            3: 'sounds/Cordes/Exercice1/3.m4a',
+            4: 'sounds/Cordes/Exercice1/4.m4a',
+            5: 'sounds/Cordes/Exercice1/5.mp3'
+        }
+    },
+    {
+        exerciseName: "Cordes - Exercice 2",
+        sounds: {
+            1: 'sounds/Cordes/Exercice2/1.m4a',
+            2: 'sounds/Cordes/Exercice2/2.mp3',
+            3: 'sounds/Cordes/Exercice2/3.m4a',
+            4: 'sounds/Cordes/Exercice2/4.m4a',
+            5: 'sounds/Cordes/Exercice2/5.m4a'
+        }
+    },
+    // PERCUSSIONS (6 sounds)
+    {
+        exerciseName: "Percussions - Exercice 1",
+        sounds: {
+            1: 'sounds/Percussions/Exercice1/1.m4a',
+            2: 'sounds/Percussions/Exercice1/2.m4a',
+            3: 'sounds/Percussions/Exercice1/3.m4a',
+            4: 'sounds/Percussions/Exercice1/4.mp3',
+            5: 'sounds/Percussions/Exercice1/5.m4a',
+            6: 'sounds/Percussions/Exercice1/6.mp3'
+        }
+    },
+    {
+        exerciseName: "Percussions - Exercice 2",
+        sounds: {
+            1: 'sounds/Percussions/Exercice2/1.mp3',
+            2: 'sounds/Percussions/Exercice2/2.mp3',
+            3: 'sounds/Percussions/Exercice2/3.m4a',
+            4: 'sounds/Percussions/Exercice2/4.m4a',
+            5: 'sounds/Percussions/Exercice2/5.m4a',
+            6: 'sounds/Percussions/Exercice2/6.m4a'
+        }
+    },
+    // VENTS (4 sounds)
+    {
+        exerciseName: "Vents - Exercice 1",
+        sounds: {
+            1: 'sounds/Vents/Exercice 1/1.m4a',
+            2: 'sounds/Vents/Exercice 1/2.m4a',
+            3: 'sounds/Vents/Exercice 1/3.m4a',
+            4: 'sounds/Vents/Exercice 1/4.m4a'
+        }
+    },
+    {
+        exerciseName: "Vents - Exercice 2",
+        sounds: {
+            1: 'sounds/Vents/Exercice 2/1.m4a',
+            2: 'sounds/Vents/Exercice 2/2.m4a',
+            3: 'sounds/Vents/Exercice 2/3.m4a',
+            4: 'sounds/Vents/Exercice 2/4.m4a'
+        }
+    }
+];
